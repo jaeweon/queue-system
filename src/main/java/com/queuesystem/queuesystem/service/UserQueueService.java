0e1d5456e9d9f106eecc0890f6801015126db72e
jaeweon <fhfkfk789@naver.com>
2025-01-20 17:36:03 +0900

    접속 허용 스케줄러

--- src/main/java/com/queuesystem/queuesystem/service/UserQueueService.java
+++ src/main/java/com/queuesystem/queuesystem/service/UserQueueService.java
@@ -3,21 +3,28 @@ package com.queuesystem.queuesystem.service;
 import com.queuesystem.queuesystem.exception.ErrorCode;
 import com.queuesystem.queuesystem.utils.RedisUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
 import java.time.Instant;
 
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserQueueService {
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
     private final RedisUtils redisUtils;
-    private final String USER_QUEUE_WAIT_KEY = "user_queue:%s:wait";
-    private final String USER_QUEUE_PROCEED_KEY = "user_queue:%s:proceed";
+    private final String USER_QUEUE_WAIT_KEY = "user:queue:%s:wait";
+    private final String USER_QUEUE_WAIT_FOR_SCAN = "user:queue:*:wait";
+    private final String USER_QUEUE_PROCEED_KEY = "user:queue:%s:proceed";
 
     // 대기열 등록
     public Mono<Long> registerWaitQueue(final String queue, final Long userId) {
@@ -30,10 +37,10 @@ public class UserQueueService {
                         : redisUtils.addUserToQueue(key, userId, unixTimestamp));
     }
 
-    // 대기열 진입 가능 여부
+
     public Mono<Long> allowUser(final String queue, final Long count) {
         return reactiveRedisTemplate.opsForZSet().popMin(USER_QUEUE_WAIT_KEY.formatted(queue), count)
-                .flatMap(user -> reactiveRedisTemplate.opsForZSet().add(USER_QUEUE_PROCEED_KEY.formatted(queue), user.getValue(), Instant.now().getEpochSecond()))
+                .flatMap(member -> reactiveRedisTemplate.opsForZSet().add(USER_QUEUE_PROCEED_KEY.formatted(queue), member.getValue(), Instant.now().getEpochSecond()))
                 .count();
     }
 
@@ -48,5 +55,20 @@ public class UserQueueService {
         return reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString())
                 .defaultIfEmpty(-1L)
                 .map(rank -> rank >= 0 ? rank + 1 : rank);
+    }
+
+    @Scheduled(initialDelay = 5000, fixedDelay = 10000)
+    public void scheduleAllowUser() {
+//        log.info("접속 허용 중...");
+
+        var maxAllowUserCount = 3L;
+        reactiveRedisTemplate.scan(ScanOptions.scanOptions()
+                        .match(USER_QUEUE_WAIT_FOR_SCAN)
+                        .count(100)
+                        .build())
+                .map(key -> key.split(":")[2])
+                .flatMap(queue -> allowUser(queue, maxAllowUserCount).map(allowed -> Tuples.of(queue, allowed)))
+                .doOnNext(tuple -> log.info("Tried %d and allowed %d members of %s queue".formatted(maxAllowUserCount, tuple.getT2(), tuple.getT1())))
+                .subscribe();
     }
 }
